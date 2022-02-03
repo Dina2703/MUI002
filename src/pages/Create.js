39a0573809be3f70d001126ae01ac4b7782a304d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-03 17:49:52 -0500

    Buttons MUI

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Button, ButtonGroup, Container } from "@material-ui/core";
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         component="h2"
@@ -12,6 +12,29 @@ export default function Create() {
       >
         Create a new Note
       </Typography>
-    </div>
+      <Button
+        type="submit"
+        color="secondary"
+        variant="contained"
+        onClick={() => console.log("clicked")}
+      >
+        Submit
+      </Button>
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => alert("Hello")}
+        >
+          Click
+        </Button>
+        <Button variant="contained" href="https://google.com">
+          Link
+        </Button>
+      </ButtonGroup>
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+    </Container>
   );
 }
