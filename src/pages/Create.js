aeaee6b9fc8c94d15ca1cb70155fc8572953f0a4
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-04 12:53:18 -0500

    JSON Server

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -12,6 +12,7 @@ import {
   FormControl,
 } from "@material-ui/core";
 import SendTwoToneIcon from "@material-ui/icons/SendTwoTone";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   field: {
@@ -26,6 +27,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
   const [titleError, setTitleError] = useState(false);
@@ -44,7 +46,15 @@ export default function Create() {
       setDetailsError(true);
     }
     if (title && details) {
-      console.log(title, details, category);
+      fetch("http://localhost:8000/notes", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          title,
+          details,
+          category,
+        }),
+      }).then(() => history.push("/"));
     }
   };
 
