e99db2156a57ee13b948059f8a6b1b588743da54
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-03 23:33:51 -0500

    TextField and error validation for them

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,49 @@
-import React from "react";
-import { Typography, Button, Container, makeStyles } from "@material-ui/core";
+import { useState } from "react";
+import {
+  Typography,
+  Button,
+  Container,
+  makeStyles,
+  TextField,
+} from "@material-ui/core";
 import SendTwoToneIcon from "@material-ui/icons/SendTwoTone";
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 40,
+    display: "block",
+  },
+  btn: {
+    margin: 10,
+  },
+});
 
 export default function Create() {
   const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDetailsError(false);
+    setTitleError(false);
+    if (title && details) {
+      console.log(title, details);
+    }
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (details === "") {
+      setDetailsError(true);
+    }
+    if (details === "" && title === "") {
+      setDetailsError(true);
+      alert("Pleadse enter the Title and Details");
+    }
+  };
   return (
     <Container>
       <Typography
@@ -17,25 +55,41 @@ export default function Create() {
         Create a new Note
       </Typography>
 
-      <Button
-        type="submit"
-        color="primary"
-        variant="contained"
-        onClick={() => console.log("clicked")}
-        endIcon={<SendTwoToneIcon />}
-      >
-        Submit
-      </Button>
-      <br />
-      <Button
-        type="submit"
-        color="secondary"
-        variant="contained"
-        onClick={() => console.log("clicked")}
-        endIcon={<SendTwoToneIcon />}
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          error={titleError}
+        />
+        <TextField
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          required
+          fullWidth
+          multiline
+          rows={4}
+          onChange={(e) => setDetails(e.target.value)}
+          error={detailsError}
+        />
+
+        <Button
+          className={classes.btn}
+          type="submit"
+          color="secondary"
+          variant="contained"
+          onClick={() => console.log("clicked")}
+          endIcon={<SendTwoToneIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
