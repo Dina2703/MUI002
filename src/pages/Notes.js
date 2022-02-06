08c4e75f27a185437693f66eb88f2d5ac60adfab
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-05 20:54:51 -0500

    fixing  bugs

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -35,7 +35,7 @@ export default function Notes() {
         columnClassName="my-masonry-grid_column"
       >
         {notes.map((note) => (
-          <div item key={note.id}>
+          <div key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete} />
           </div>
         ))}
